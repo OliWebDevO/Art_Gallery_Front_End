178bac8a883e5ae20cba362b242231980e0e761e
OliWebDevO <oliver-vdb@hotmail.com>
2024-06-06 18:35:47 +0200

    register done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,17 +1,43 @@
+import { useState } from 'react'
 import './register.scss'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 const Register = () => {
+
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:"",
+        name:"",
+    })
+    const [error, setError] = useState(false)
+
+    const handleChange = (e) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+
+        try {
+            await axios.post('http://localhost:8800/api/auth/register', inputs)
+        } catch(err) {
+            setError(err.response.data)
+        }
+    }
+
     return (
         <div className='register'>
             <div className="card">
                 <div className="left">
                     <h1>Register</h1>  
                     <form action="">
-                        <input type="text" placeholder='Username'/>
-                        <input type="text" placeholder='Name'/>
-                        <input type='mail' placeholder='Email'/>
-                        <input type="password" placeholder='Password'/>
-                        <button>Register</button>
+                        <input type="text" placeholder='Username' name='username' onChange={handleChange}/>
+                        <input type="text" placeholder='Name' name='email' onChange={handleChange}/>
+                        <input type='mail' placeholder='Email' name='password' onChange={handleChange}/>
+                        <input type="password" placeholder='Password' name='name' onChange={handleChange}/>
+                        {error && <div className='error'>{error}</div>}
+                        <button onClick={handleClick}>Register</button>
                         <Link to={'/login'}>
                         <button className='login-btn'>Login</button>
                         </Link>
